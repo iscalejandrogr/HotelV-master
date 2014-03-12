57a79ab5c9949439c884ca9aa5d88a3dc1aaecb8
Alejandro Guzmán <i.s.c.alejandro.gr@gmail.com>
2014-03-11 21:58:52 -0600

    actualizacion
    nueva actualizacion

--- js/API/archivos.js
+++ js/API/archivos.js
@@ -1,8 +1,9 @@
-//archivos
-function uploadPhoto(foto, nom) {
+// archivo
+
+function uploadPhoto(foto,nom) {
 	var options = new FileUploadOptions();
 	options.fileKey="archivo";
-	options.fileName="Carlos";
+	options.fileName="Kch";
 	options.mimeType="image/jpeg";
 
 	var params = {};
@@ -12,24 +13,21 @@ function uploadPhoto(foto, nom) {
 	options.params = params;
 
 	var ft = new FileTransfer();
-	ft.upload(foto,"http://igitsoft.com/pgtest.php",function(r){
+	ft.upload(foto, "http://igitsoft.com/pgtest.php", function (r) {
 		saveLogin(nom,disp()['id']);
-		navigator.notification.confirm("Datos Guardos Satisfactoriamente\n"+disp()['platform']+'\n'+r.response, function(botones){
-			switch(botones){
-				case 1:
-					navigator.notification.beep(5);
-					window.location.href='#page';
-					break;
-				case 2:
-					navigator.notification.vibrate(500);
-					window.location.href='#page';
-					break;	
-				case 3:
-					window.location.href='#page';
-					break;
-			}
-		}, "Titulo", "Beep, Vibrar, Salir");
-	}, function(err){
-		navigator.notification.alert("Error: "+err.code, null, "Error de Envío", "De acuerdo");
-	}, options);
-}
+		 navigator.notification.confirm("Datos Guardados Satisfactoriamente\n"+disp()['platform'+'\n'+r.response],function(botones){
+		switch(botones){
+		case 1:
+		navigator.notification.beep(5);
+		break;
+		case 2:
+		navigator.notification.vibrate(500);
+		break;
+		}
+	},"Titulo","Beep,Vibrar,Salir");
+				
+				}, function(err){
+					
+					navigator.notification.alert("Error: "+err.code, null,"Error de Envio","De acuerdo");
+					}, options);
+        }

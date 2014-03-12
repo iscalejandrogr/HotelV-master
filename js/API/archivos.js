0f9714750a25682b0fdced25af52889036cdab8a
Alejandro Guzmán <i.s.c.alejandro.gr@gmail.com>
2014-03-11 22:38:32 -0600

    nueva actualizacion
    esta ves tiene que hacer algo

--- js/API/archivos.js
+++ js/API/archivos.js
@@ -1,9 +1,8 @@
-// archivo
-
-function uploadPhoto(foto,nom) {
+//archivos
+function uploadPhoto(foto, nom) {
 	var options = new FileUploadOptions();
 	options.fileKey="archivo";
-	options.fileName="Kch";
+	options.fileName="Carlos";
 	options.mimeType="image/jpeg";
 
 	var params = {};
@@ -13,21 +12,24 @@ function uploadPhoto(foto,nom) {
 	options.params = params;
 
 	var ft = new FileTransfer();
-	ft.upload(foto, "http://igitsoft.com/pgtest.php", function (r) {
+	ft.upload(foto,"http://igitsoft.com/pgtest.php",function(r){
 		saveLogin(nom,disp()['id']);
-		 navigator.notification.confirm("Datos Guardados Satisfactoriamente\n"+disp()['platform'+'\n'+r.response],function(botones){
-		switch(botones){
-		case 1:
-		navigator.notification.beep(5);
-		break;
-		case 2:
-		navigator.notification.vibrate(500);
-		break;
-		}
-	},"Titulo","Beep,Vibrar,Salir");
-				
-				}, function(err){
-					
-					navigator.notification.alert("Error: "+err.code, null,"Error de Envio","De acuerdo");
-					}, options);
-        }
+		navigator.notification.confirm("Datos Guardos Satisfactoriamente\n"+disp()['platform']+'\n'+r.response, function(botones){
+			switch(botones){
+				case 1:
+					navigator.notification.beep(5);
+					window.location.href='#page';
+					break;
+				case 2:
+					navigator.notification.vibrate(500);
+					window.location.href='#page';
+					break;	
+				case 3:
+					window.location.href='#page';
+					break;
+			}
+		}, "Titulo", "Beep, Vibrar, Salir");
+	}, function(err){
+		navigator.notification.alert("Error: "+err.code, null, "Error de Envío", "De acuerdo");
+	}, options);
+}
--- js/API/captura.js
+++ js/API/captura.js
@@ -1,14 +1,9 @@
-// Captura
-function tomarFoto()
-{
-	// start image capture
-	navigator.device.capture.captureImage(function(mediaFiles) {
-	var i, path, len;
-	
+//captura
+function tomarFoto(){
+	navigator.device.capture.captureImage(function(mediaFiles){
 		path = mediaFiles[0].fullPath;
-		$('#regFoto').append('<img src="'+path+'" width="100%"/>').attr('rel',path);
-		},  function(error) {
+		$('#regFoto').append('<br><img src="'+path+'" width="100%" />').attr('rel',path);
+	},function(error){
 		navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
-	}, {limit:2});
-
+	},{limit:1});
 }
